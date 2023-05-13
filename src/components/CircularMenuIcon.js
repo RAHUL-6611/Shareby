311f4174c36bbb9fb99cfc9475e0cc90510f6538
rahul6611 <rahulparmar7654321@gmail.com>
2023-05-13 17:07:00 +0530

    done

---
+++ src/components/CircularMenuIcon.js
@@ -0,0 +1,9 @@
+import {FcShare} from "react-icons/fc";
+function CircularMenuIcon(){
+    return (
+        <div class="circular-menu-icon" >
+          <FcShare style={{ fontSize: '400%'}} class="icon"/>
+        </div>
+    );
+}
+export default CircularMenuIcon;
